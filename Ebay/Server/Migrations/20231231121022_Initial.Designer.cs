1a7a5448bea3bff4d4125dec3a36b927f4f9d756
Artem Petrov <apetrov@ptsecurity.com>
2023-12-31 19:12:56 +0700

    Добавил миграцию бд

--- Ebay/Server/Migrations/20231231121022_Initial.Designer.cs
+++ Ebay/Server/Migrations/20231231121022_Initial.Designer.cs
@@ -3,17 +3,19 @@ using System;
 using Ebay.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace Ebay.Server.Data.Migrations
+namespace Ebay.Server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231231121022_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
