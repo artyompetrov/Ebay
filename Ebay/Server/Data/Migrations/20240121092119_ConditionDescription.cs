eed4118eec0d8878be017a13818421d4957d66e2
Artem Petrov <apetrov@ptsecurity.com>
2024-01-21 16:21:51 +0700

    Коммит

---
+++ Ebay/Server/Data/Migrations/20240121092119_ConditionDescription.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ebay.Server.Data.Migrations
+{
+    public partial class ConditionDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ConditionDescription",
+                table: "Lots",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ConditionDescription",
+                table: "Lots",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
