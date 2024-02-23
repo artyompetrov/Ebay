07b77cabbe241d745858ce5c184bd7dfd6f855d0
Artem Petrov <apetrov@ptsecurity.com>
2024-02-24 00:46:33 +0700

    Коммит

--- Python/export_to_html.py
+++ Python/export_to_html.py
@@ -1,6 +1,13 @@
 import nbformat
+from traitlets.config import Config
 from nbconvert.preprocessors import ExecutePreprocessor
 from nbconvert import HTMLExporter
+c = Config()
+c.TagRemovePreprocessor.remove_cell_tags = ("remove_cell",)
+c.TagRemovePreprocessor.remove_all_outputs_tags = ("remove_output",)
+c.TagRemovePreprocessor.remove_input_tags = ("remove_input",)
+c.TagRemovePreprocessor.enabled = True
+c.HTMLExporter.preprocessors = ["nbconvert.preprocessors.TagRemovePreprocessor"]
 
 # read source notebook
 with open('Prices.ipynb') as f:
@@ -11,7 +18,7 @@ ep = ExecutePreprocessor(timeout=-1, kernel_name='python3')
 ep.preprocess(nb)
 
 # export to html
-html_exporter = HTMLExporter()
+html_exporter = HTMLExporter(config=c)
 html_exporter.exclude_input = True
 html_data, resources = html_exporter.from_notebook_node(nb)
 
