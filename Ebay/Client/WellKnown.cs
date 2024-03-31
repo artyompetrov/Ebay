407c0360a203c5d7d3677e07ddc769588b879db5
Artem Petrov <apetrov@ptsecurity.com>
2024-04-01 01:39:27 +0700

    Коммит

--- Ebay/Client/WellKnown.cs
+++ Ebay/Client/WellKnown.cs
@@ -1,19 +1,26 @@
+using Ebay.Client.Pages;
+
 namespace Ebay.Client;
 
 internal class WellKnown
 {
     public const int RecheckTimeInDays = 90;
-    
+
     public static class Categories
     {
         public static class Conditions
         {
             public static string CategoryName = "condition";
         }
-    
+
         public static class TestState
         {
             public static string CategoryName = "test_state";
         }
+    }
+
+    public static class Currencies
+    {
+        public static string TargetCurrency = "RUB";
     }
 }
