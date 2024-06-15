2ed7775edc157483438b8903bf19bbd38b92fe4a
Artem Petrov <apetrov@ptsecurity.com>
2024-06-15 18:04:14 +0700

    Коммит

--- deploy/apache/start-apache.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Используем envsubst для замены переменных окружения в конфигурационном файле
-envsubst < /etc/apache2/sites-available/000-default.conf.template > /etc/apache2/sites-available/000-default.conf
-
-# Запускаем Apache
-httpd-foreground
