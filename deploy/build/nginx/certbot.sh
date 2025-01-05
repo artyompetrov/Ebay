5b447bb03ea558fe1cbe0191297b220bdb874173
Artem Petrov <apetrov@ptsecurity.com>
2025-01-05 18:38:04 +0700

    Включил certbot в контейнер с nginx

--- deploy/build/nginx/certbot.sh
+++ deploy/build/nginx/certbot.sh
@@ -1,5 +1,12 @@
-﻿#!/bin/sh
-set -e
+﻿set -e
+
+echo "Creating certificate..."
+certbot certonly \
+  --standalone \
+  -d $DOMAIN -d www.$DOMAIN \
+  --email $EMAIL \
+  --agree-tos \
+  --no-eff-email
 
 echo "Starting crond..."
 crond
