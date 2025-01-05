be9664dbdd7ac8ed5627a287a5eb3f70932c4ea5
Artem Petrov <apetrov@ptsecurity.com>
2025-01-05 23:57:13 +0700

    Добавил упаковку расширения

--- src/Ebay/ChromeExtension/webpack.config.js
+++ src/Ebay/ChromeExtension/webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
         ],
     },
     output: {
-        path: path.resolve(__dirname, "../../EbayChromeExtension/build/"),
+        path: path.resolve(__dirname, "./_extension/build/"),
         filename: "ebay.js"
     },
     mode: 'development',
