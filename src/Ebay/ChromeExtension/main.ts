fd7dd0416b40096e93d83e5e0d33187192467417
Artem Petrov <apetrov@ptsecurity.com>
2025-01-06 17:32:08 +0700

    Опубликовал новую версию расширения

--- src/Ebay/ChromeExtension/main.ts
+++ src/Ebay/ChromeExtension/main.ts
@@ -873,6 +873,56 @@ function fillLotInfo(ebayItem: Item, shippingCountry: string, shippingCostXml: s
 }
 
 
+async function checkForUpdates(): Promise<void> {
+    const currentVersion = chrome.runtime.getManifest().version;
+    const updateUrl = chrome.runtime.getManifest().update_url;
+
+    if (!updateUrl) {
+        throw new Error("Update URL not defined in manifest.");
+    }
+    const response = await fetchResource(updateUrl, {});
+    if (!response.ok) {
+        throw new Error("Failed to fetch update manifest: " + response.statusText);
+    }
+
+    const xmlText = await response.text();
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(xmlText, "application/xml");
+    
+    const updateCheckNode = xmlDoc.querySelector("updatecheck");
+    const remoteVersion = updateCheckNode?.getAttribute("version");
+
+    if (!remoteVersion) {
+        console.error("Version not found in the update manifest.");
+        return;
+    }
+
+    const isNewerVersion = (current: string, remote: string): boolean => {
+        const currentParts = current.split('.').map(Number);
+        const remoteParts = remote.split('.').map(Number);
+
+        for (let i = 0; i < Math.max(currentParts.length, remoteParts.length); i++) {
+            const currentPart = currentParts[i] || 0;
+            const remotePart = remoteParts[i] || 0;
+
+            if (remotePart > currentPart) {
+                return true;
+            } else if (remotePart < currentPart) {
+                return false;
+            }
+        }
+
+        return false;
+    };
+    
+    if (isNewerVersion(currentVersion, remoteVersion)) {
+        alert(`Ebay Helper extension New version available: ${remoteVersion}`);
+    }
+    
+    console.log("No updates available. Current version is up-to-date.");
+}
+
+
 async function getDataFromPage(backendClient: EbayToolBackendClient, ebayClient: EbayClient, ebayShoppingApiClient: EbayShoppingApiClient) {
     await Promise.all([
         await getEbayItem(ebayClient, ebayShoppingApiClient),
@@ -1108,6 +1158,8 @@ async function saveCodeVerifier() {
 export async function run() {
     await sleepElementLoaded('footer', document)
     await saveCodeVerifier();
+
+    await checkForUpdates();
 
     let ebayOAuth2Client = new OAuth2Client({
         server: "https://auth.ebay.com/",
