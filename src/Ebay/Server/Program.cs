5ebca2808619ebe1ecaf2f34429bdfda436dab84
Artem Petrov <apetrov@ptsecurity.com>
2025-01-08 20:27:53 +0700

    добавил ссылку на скачивание плагина

--- src/Ebay/Server/Program.cs
+++ src/Ebay/Server/Program.cs
@@ -108,7 +108,8 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    app.UseExceptionHandler("/Error");
+    //TODO скрыть явное отображение ошибок
+    //app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
