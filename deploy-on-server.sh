b2c4c262a7222d7a98800aa73b628d493499e754
Artem Petrov <apetrov@ptsecurity.com>
2025-01-08 13:09:00 +0700

    добавил prune

--- deploy-on-server.sh
+++ deploy-on-server.sh
@@ -6,3 +6,4 @@ env $(cat server.env | xargs) docker compose pull
 docker compose --env-file server.env build
 docker compose --env-file server.env down
 docker compose --env-file server.env up -d
+docker image prune -f
