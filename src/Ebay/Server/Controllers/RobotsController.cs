1ac6ac11e841dae36d00f5ac175fa1ba91411632
Artem Petrov <apetrov@ptsecurity.com>
2025-01-08 20:43:52 +0700

    добавил robots.txt

---
+++ src/Ebay/Server/Controllers/RobotsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers;
+
+[ApiController]
+public class RobotsController : ControllerBase
+{
+    [HttpGet("/robots.txt")]
+    public ContentResult RobotsTxt()
+    {
+        const string content = "User-agent: *\nDisallow: /";
+
+        return Content(content, "text/plain");
+    }
+}
