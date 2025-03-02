ca1a5a6e1d4bd12e9c44ac5e2ea4ffca1948dc94
Artem Petrov <apetrov@ptsecurity.com>
2025-03-03 01:41:17 +0700

    frappe

--- src/frappe_ebay/ebay/ebay/doctype/ebayproduct/ebayproduct_list.js
+++ src/frappe_ebay/ebay/ebay/doctype/ebayproduct/ebayproduct_list.js
@@ -1,14 +1,43 @@
 ﻿frappe.listview_settings['EbayProduct'] = {
 
-    button: {
-        show(doc) {
-            return doc.reference_name;
-        },
-        get_label() {
-            return 'View';
-        },
-        action(doc) {
-            frappe.set_route('Form', doc.reference_type, doc.reference_name);
-        }
+    
+    onload(listview) {
+        document.querySelectorAll('.list-row-col').forEach(function (coll) {
+            coll.style.minWidth = "120px";
+        });
+        document.querySelectorAll('.list-subject').forEach(function (coll) {
+            coll.style.minWidth = "120px";
+        });
+        document.querySelectorAll('.frappe-list').forEach(function (coll) {
+            coll.style.overflowX = "auto";
+        });
+        
+        document.querySelectorAll('.list-row .level-right').forEach(function (coll) {
+            coll.style.flex = "0";
+        });
+        
+        console.log("tezt")
     },
+    formatters: {
+        links(val,value_doc,doc) {
+
+            let id = "links_"+doc.name;
+            
+            frappe.db.get_doc('EbayProduct', doc.name).then(function(r) {
+                let items = r.ebay_search_names.map(x => {
+                    const query = encodeURIComponent(x.ebay_name);
+                    const url = `https://www.ebay.com/sch/i.html?_nkw=${query}`;
+                    return `<a href="${url}" target="_blank" rel="noopener noreferrer">${x.ebay_name}</a>`;
+                });
+                
+                let element = document.querySelector("#" + id);
+                if (element) {
+                    element.innerHTML = items.join(" ");
+                }
+                 
+            })
+            
+            return  `<div id='${id}'></div>`;
+        }
+    }
 }
