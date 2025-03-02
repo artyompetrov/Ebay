852a3b3a08cec1720ad23f7cc99e8625d9fc342c
Artem Petrov <apetrov@ptsecurity.com>
2025-03-02 21:46:36 +0700

    frappe

---
+++ src/frappe_ebay/ebay/ebay/doctype/ebayproduct/ebayproduct_list.js
@@ -0,0 +1,14 @@
+﻿frappe.listview_settings['EbayProduct'] = {
+
+    button: {
+        show(doc) {
+            return doc.reference_name;
+        },
+        get_label() {
+            return 'View';
+        },
+        action(doc) {
+            frappe.set_route('Form', doc.reference_type, doc.reference_name);
+        }
+    },
+}
--- src/frappe_ebay/ebay/hooks.py
+++ src/frappe_ebay/ebay/hooks.py
@@ -44,7 +44,11 @@ app_license = "mit"
 
 # include js in doctype views
 # doctype_js = {"doctype" : "public/js/doctype.js"}
-# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
+doctype_list_js = {
+        "EbayProduct" : [
+            "ebay/doctype/ebayproduct/ebayproduct_list.js"
+        ]
+    }
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
 
