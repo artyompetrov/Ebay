dc5deb723cbc0a0ab06db4e8f7d2fb85015a97c1
Artem Petrov <apetrov@ptsecurity.com>
2025-03-30 17:03:01 +0700

    рефакторинг

--- src/ChromeExtension/src/constants.ts
+++ src/ChromeExtension/src/constants.ts
@@ -8,9 +8,7 @@
 export const Urls = {
     backendUrl: `https://tubessale.ddns.net/`,
     baseApiUrl: `https://tubessale.ddns.net/api/ebay/v1`,
-    frappeBaseApiUrl: "https://tubessale.ddns.net:8081",
     extensionAuthRedirectUrl: `https://tubessale.ddns.net/chrome_extensions/auth`,
-    frappeAuthRedirectUrl: `https://tubessale.ddns.net:8081/api/method/ebay.api.chrome_extension_auth_page.auth`,
     ebayAuthRedirectUrl: `https://www.ebay.com/`
 };
 
@@ -18,7 +16,6 @@ export const Auth = {
     ebayRedirectUriCode: "Artem_Petrov-ArtemPet-tubesS-dsrgu",
     ebayApiScope: "https://api.ebay.com/oauth/api_scope",
     backendApiScope: 'ServerAPI',
-    frappeScope: 'all openid'
 };
 
 export const Settings = {
