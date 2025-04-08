77e874ec600902de413c7362e05499f3640ee83e
Artem Petrov <apetrov@ptsecurity.com>
2025-04-08 18:44:18 +0700

    поправил регулярки

--- src/ChromeExtension/src/siteProcessors/SearchSitesProcessor.ts
+++ src/ChromeExtension/src/siteProcessors/SearchSitesProcessor.ts
@@ -129,7 +129,7 @@ class SearchSitesProcessor implements ISiteProcessor {
     transliterate(text: string): string {
         const map: { [key: string]: string } = {
             "а": "a", "б": "b", "в": "v", "г": "g", "д": "d", "е": "e", "ё": "yo",
-            "ж": "J", "з": "z", "и": "i", "й": "y", "к": "k", "л": "l", "м": "m",
+            "ж": "j", "з": "z", "и": "i", "й": "y", "к": "k", "л": "l", "м": "m",
             "н": "n", "о": "o", "п": "p", "р": "r", "с": "s", "т": "t", "у": "u",
             "ф": "f", "х": "h", "ц": "c", "ч": "ch", "ш": "sh", "щ": "sh",
             "ъ": "", "ы": "y", "ь": "", "э": "e", "ю": "yu", "я": "ya",
