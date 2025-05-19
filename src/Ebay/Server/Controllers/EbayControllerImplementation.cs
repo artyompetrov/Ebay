f2293e024965c707b4ce7935cccb171b1e86fc95
Artem Petrov <apetrov@ptsecurity.com>
2025-05-19 12:42:30 +0700

    добавил plate curves

--- src/Ebay/Server/Controllers/EbayControllerImplementation.cs
+++ src/Ebay/Server/Controllers/EbayControllerImplementation.cs
@@ -326,7 +326,7 @@ internal class EbayControllerImplementation : IEbayController
             {
                 hashPlateCurves = await ComputeEntryHashAsync(entry, cancellationToken);
             }
-            else if (fileName.EndsWith("quick_test.txt", StringComparison.Ordinal))
+            else if (fileName.EndsWith("uts.txt", StringComparison.Ordinal))
             {
                 hashQuickTest = await ComputeEntryHashAsync(entry, cancellationToken);
             }
