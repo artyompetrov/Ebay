99a23e86f353568035ce1ac03883fcbab872c12f
artyompetrov <31510247+artyompetrov@users.noreply.github.com>
2025-08-01 16:33:33 +0700

    Add batch ID field for measurements (#73)

---
+++ src/Ebay/Server.Application/Migrations/20250801090139_AddBatchIdToMeasurement.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBatchIdToMeasurement : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BatchId",
+                table: "ProductMeasurements",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BatchId",
+                table: "ProductMeasurements");
+        }
+    }
+}
