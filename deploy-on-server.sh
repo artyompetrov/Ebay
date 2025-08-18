97494b2c5f449253c136daea532e962002ed1b1d
Artem Petrov <apetrov@ptsecurity.com>
2025-08-18 11:33:12 +0700

    Графана

--- deploy-on-server.sh
+++ deploy-on-server.sh
@@ -3,7 +3,6 @@
 cd ./deploy || exit 1
 
 docker compose --env-file server.env pull
-docker compose --env-file server.env build
 docker compose --env-file server.env down
 docker compose --env-file server.env up -d
 docker image prune -f
