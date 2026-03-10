401252b51129bb6360f67ae8ad7b4fab8cd9be99
Artem Petrov <apetrov@ptsecurity.com>
2026-03-10 10:51:38 +0700

    поправил

--- deploy-on-server.sh
+++ deploy-on-server.sh
@@ -5,4 +5,5 @@ cd ./deploy || exit 1
 docker compose --env-file server.env pull
 docker compose --env-file server.env down
 docker compose --env-file server.env up -d
-docker image prune -f
+docker image prune -af
+docker builder prune -af
